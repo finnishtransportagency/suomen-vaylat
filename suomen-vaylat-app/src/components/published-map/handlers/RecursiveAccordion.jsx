74b7c294fac0cea6cdc82345a9e529f01c0d8f3b
Oskari Rintamäki <oskari.rintamaki@sitowise.com>
2021-06-14 19:36:37 +0300

    base hierarchical

--- suomen-vaylat-app/src/components/published-map/handlers/RecursiveAccordion.jsx
+++ suomen-vaylat-app/src/components/published-map/handlers/RecursiveAccordion.jsx
@@ -1,63 +1,79 @@
 
-import { Accordion } from 'react-bootstrap';
-
-var baseItems = [
-    { id: 1, title: "Land", body: "", parentId: 0 },
-    { id: 2, title: "Sea", body: "Submarine", parentId: 0 },
-    { id: 3, title: "Cars", body: "", parentId: 1 }
-  ];
+import { Accordion, Card, Button } from 'react-bootstrap';
 
 export const RecursiveAccordion = ({ items, recurse = false }) => {
     console.log(items);
     return (
-      <Accordion>
+        <div>
         {items.map((item, index) => {
-          var children = baseItems.filter((i) => {
+          var children = items.filter((i) => {
             return i.parentId === item.id;
           });
   
           var hasChildren = children.length > 0;
+          /*
+
+          var hasChildren = groups.length > 0;
+
+          if (item.parentId === -1)
+          */
   
           if (item.parentId === 0) {
             return (
-              <Accordion.Item eventKey={index}>
-              <Accordion.Header>{item.title}</Accordion.Header>
-              <Accordion.Body>
-                {hasChildren && (
-                  <div>
-                    <RecursiveAccordion items={children} recurse={true} />
-                    { item.body }
-                  </div>
-                )}
-                {!hasChildren && (
-                  <div dangerouslySetInnerHTML={{ __html: item.body }} />
-                )}
-                </Accordion.Body>
-              </Accordion.Item>
+                <Accordion>
+                <Card>
+                    <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey={item.id}>
+                        {item.title}
+                    </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey={item.id}>
+                        <Card.Body>
+                            {hasChildren && (
+                            <div>
+                                <RecursiveAccordion items={children} recurse={true} />
+                                { item.body }
+                            </div>
+                            )}
+                            {!hasChildren && (
+                            <div dangerouslySetInnerHTML={{ __html: item.body }} />
+                            )}
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                </Accordion>
             );
           }
   
           if (recurse) {
-              console.log(recurse);
             return (
-              <Accordion.Item eventKey={index}>
-              <Accordion.Header>{item.title}</Accordion.Header>
-              <Accordion.Body>
-                {hasChildren && (
-                  <div>
-                    <RecursiveAccordion items={children} recurse={true} />
-                    { item.body }
-                  </div>
-                )}
-                {!hasChildren && (
-                  <div dangerouslySetInnerHTML={{ __html: item.body }} />
-                )}
-                </Accordion.Body>
-              </Accordion.Item>
+                
+                <Accordion>
+              <Card>
+              <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey={item.id}>
+                  {item.title}
+              </Accordion.Toggle>
+              </Card.Header>
+                <Accordion.Collapse eventKey={item.id}>
+                <Card.Body>
+                    {hasChildren && (
+                    <div>
+                        <RecursiveAccordion items={children} recurse={true} />
+                        { item.body }
+                    </div>
+                    )}
+                    {!hasChildren && (
+                    <div dangerouslySetInnerHTML={{ __html: item.body }} />
+                    )}
+                </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              </Accordion>
             );
           }
         })}
-      </Accordion>
+        </div>
     );
   };
 
