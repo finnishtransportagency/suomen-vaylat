b3a668a049e6434d5cfaadcb822285ef9805b2c3
Tuomo Hautala <tuomo.hautala@sitowise.com>
2021-06-24 16:11:22 +0300

    Replace react-bootstrap code with native html list

--- suomen-vaylat-app/src/components/menus/LayerList.jsx
+++ suomen-vaylat-app/src/components/menus/LayerList.jsx
@@ -1,71 +1,28 @@
 
+import { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import { Accordion, Card, Button } from 'react-bootstrap';
+import LayerGroup from './LayerGroup';
 import Layers from './Layers';
 
-export const LayerList = ({ groups, layers, recurse = false }) => {
+export const LayerList = ({ groups, layers, recurse = false}) => {
+
     return (
-        <div>
+        <>
         {groups.map((group, index) => {
             var hasChildren = false;
             if(group.groups) {
                 hasChildren = group.groups.length > 0;
             }
-          if (group.parentId === -1) {
             return (
-                <Accordion>
-                <Card style={{borderRadius: "30px", marginBottom: "5px"}}>
-                    <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey={group.id}>
-                        {group.name}
-                    </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey={group.id}>
-                        <Card.Body>
-                            {hasChildren && (
-                            <div>
-                                <Layers groupLayers={group.layers} allLayers={layers}></Layers>
-                                <LayerList groups={group.groups} layers={layers} recurse={true} />
-                            </div>
-                            )}
-                            {!hasChildren && (
-                                <Layers groupLayers={group.layers} allLayers={layers}></Layers>
-                            )}
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                </Accordion>
+                <>
+                   <LayerGroup group={group} layers={layers} hasChildren={hasChildren} />
+                </>
+
             );
-          }
-  
-          if (recurse) {
-            return (
-                
-                <Accordion>
-              <Card style={{borderRadius: "30px", marginBottom: "5px"}}>
-              <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey={group.id}>
-                  {group.name}
-              </Accordion.Toggle>
-              </Card.Header>
-                <Accordion.Collapse eventKey={group.id}>
-                <Card.Body>
-                    {hasChildren && (
-                    <div>
-                        <Layers groupLayers={group.layers} allLayers={layers}></Layers>
-                        <LayerList groups={group.groups} layers={layers} recurse={true} />
-                    </div>
-                    )}
-                    {!hasChildren && (
-                        <Layers groupLayers={group.layers} allLayers={layers}></Layers>
-                    )}
-                </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              </Accordion>
-            );
-          }
-        })}
-        </div>
+        })
+        }
+        </>
     );
   };
 
