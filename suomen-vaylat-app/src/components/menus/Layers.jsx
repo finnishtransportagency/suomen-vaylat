7973fb187b2eaa6a9fcdfcb0d5b6d27d34324391
Oskari Rintamäki <oskari.rintamaki@sitowise.com>
2021-07-01 12:29:34 +0300

    Layerlist works otherwise but error when adding layer

--- suomen-vaylat-app/src/components/menus/Layers.jsx
+++ suomen-vaylat-app/src/components/menus/Layers.jsx
@@ -1,31 +1,13 @@
-import { useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import { ReactReduxContext, useSelector } from 'react-redux';
+import { setMapLayerVisibility } from '../../state/slices/rpcSlice';
 import styled, { keyframes } from 'styled-components';
+import Layer from './Layer';
 
-const StyledLayerContainer = styled.li`
-    overflow: hidden;
-    transition: all 0.3s ease-out;
-    //max-height: 0%;
-    display: flex;
-    align-items: center;
-    opacity: ${props => props.isOpen ? "1" : "0"};
-    height: ${props => props.isOpen ? "40px" : "0px"};
-    margin: 0;
-`;
-
-const StyledlayerHeader = styled.div`
-
-`;
-
-const StyledLayerName = styled.p`
-    font-size: 15px;
-    margin: 5px;
-`;
-
-
-
-export const Layers = ({ groupLayers, allLayers, isOpen }) => {
+export const Layers = ({ groupLayers, allLayers }) => {
     //Find matching layers from all layers and groups, then push this group's layers into 'filteredLayers'
     var filteredLayers = [];
+
     for (var i in groupLayers) {
         filteredLayers.push(allLayers.filter(layer => layer.id == groupLayers[i]));
     }
@@ -33,13 +15,7 @@ export const Layers = ({ groupLayers, allLayers, isOpen }) => {
         <>
             {filteredLayers.map((layer, index) => {
                 return (
-                    <StyledLayerContainer key={layer.id} isOpen={isOpen}>
-                        <StyledlayerHeader>
-                            <StyledLayerName>
-                                {layer[0].name}
-                            </StyledLayerName>
-                        </StyledlayerHeader>
-                    </StyledLayerContainer>
+                    <Layer layer={layer}></Layer>
             )})}
         </>
     );
