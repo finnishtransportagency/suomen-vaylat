8f2b2c46020f57cd3c525b0b353ce0c39061f171
rene-sitowise <rene-sitowise@sitowise.com>
2021-09-15 13:00:33 +0300

    SV-53 Set info button to header and and info modal on content page

---
+++ suomen-vaylat-app/src/components/app-info-modal/AppInfoModal.jsx
@@ -0,0 +1,108 @@
+import React, {useContext} from 'react';
+import strings from "../../translations";
+import styled from 'styled-components';
+import Modal from "react-modal";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import {useAppSelector} from "../../state/hooks";
+import {ReactReduxContext} from "react-redux";
+import {setIsInfoOpen} from "../../state/slices/uiSlice";
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-25%',
+        transform: 'translate(-50%, -50%)',
+        padding: '0',
+        borderRadius: 0,
+        boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
+        border: 'none'
+    },
+    overlay: {zIndex: 20}
+};
+
+const StyledContent = styled.div`
+    padding: .5rem;
+`;
+const StyledHeader = styled.div`
+    padding: .5rem;
+    background-color: ${props => props.theme.colors.maincolor1};
+    color: ${props => props.theme.colors.mainWhite};
+    border-radius: 0
+`;
+const StyledFooter = styled.div`
+    justify-content: space-between;
+`;
+
+const StyledLayerCloseIcon = styled.div`
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 28px;
+    min-height: 28px;
+    svg {
+        transition: all 0.1s ease-out;
+        font-size: 18px;
+        color: ${props => props.theme.colors.mainWhite};
+    };
+    &:hover {
+        svg {
+            color: ${props => props.theme.colors.maincolor2};
+        }
+    }
+`;
+
+const StyledOkButton = styled.div`
+    cursor: pointer;
+    display: flex;
+    justify-content: right;
+    align-items: center;
+    `;
+
+export const AppInfoModal = () => {
+    const { store } = useContext(ReactReduxContext);
+    const isInfoOpen = useAppSelector((state) => state.ui.isInfoOpen);
+    const headingText = strings.appInfo.headingText.bold()
+    const mainText = strings.appInfo.mainText
+    const content = <div dangerouslySetInnerHTML={{__html: headingText + "<br><br>" + mainText}}></div>
+    const title = strings.appInfo.title
+
+    function closeModal() {
+        store.dispatch(setIsInfoOpen(false));
+    }
+
+    return (
+        <div>
+            <Modal
+                isOpen={isInfoOpen}
+                onRequestClose={() => closeModal()}
+                style={customStyles}
+            >
+                <StyledHeader className="modal-header">
+                    <h5>{title}</h5>
+                    <StyledLayerCloseIcon
+                        onClick={() => {
+                            closeModal();
+                        }} title='Sulje'>
+                        <FontAwesomeIcon
+                            icon={faTimes}
+                        />
+                    </StyledLayerCloseIcon>
+                </StyledHeader>
+                <StyledContent>
+                    {content}
+                </StyledContent>
+                <StyledFooter className="modal-footer">
+                    <StyledOkButton>
+                        <button onClick={() => closeModal()}>OK</button>
+                    </StyledOkButton>
+                </StyledFooter>
+            </Modal>
+        </div>
+    );
+}
+export default AppInfoModal;
