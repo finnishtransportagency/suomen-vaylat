7aa237af65b5441b08143e1c5e20192479771696
Tuomo Hautala <tuomo.hautala@sitowise.com>
2021-09-15 14:34:30 +0300

    Disable not working functionality

--- suomen-vaylat-app/src/components/menus/hierarchical-layerlist/LayerSearch.jsx
+++ suomen-vaylat-app/src/components/menus/hierarchical-layerlist/LayerSearch.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import styled from 'styled-components';
+import strings from '../../../translations';
 
 const StyledLayerSearchContainer = styled.div`
     display: flex;
@@ -7,15 +7,18 @@ const StyledLayerSearchContainer = styled.div`
 `;
 
 const StyledSearchInput = styled.input`
+    cursor: not-allowed;
     text-align: middle;
     border: 1px solid black;
+    border: 1px solid rgba(0, 0, 0, 0.2);
     border-radius: 5px;
     width: 100%;
     margin: 10px 15px 0px 15px;
     height: 40px;
     &::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
         padding-left: 10px;
-        color: #000;
+        //color: #000;
+        color: rgba(0, 0, 0, 0.2);
     }
 `;
 
@@ -23,7 +26,7 @@ const StyledSearchInput = styled.input`
 const LayerSearch = () => {
     return (
         <StyledLayerSearchContainer>
-                    <StyledSearchInput type="text" placeholder="Hae karttatasoja..." />
+                    <StyledSearchInput type="text" placeholder={strings.layerlist.layerlistLabels.searchForLayers} />
         </StyledLayerSearchContainer>
     )
 };
