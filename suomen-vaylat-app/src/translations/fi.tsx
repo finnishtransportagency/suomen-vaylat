634e33e3c390ed43978827269ec3d8c81e956d43
rene-sitowise <rene-sitowise@sitowise.com>
2021-11-30 18:01:00 +0200

    SV-199 Set css, tranlations

--- suomen-vaylat-app/src/translations/fi.tsx
+++ suomen-vaylat-app/src/translations/fi.tsx
@@ -1,3 +1,5 @@
+import {Accordion} from "react-bootstrap";
+
 const fi = {
     language: {
         languageSelection: {
@@ -29,7 +31,8 @@ const fi = {
         share: 'Jaa sivu',
         shareTheme: 'Jaa teema',
         metadata: 'Näytä tason metadata',
-        showPageInfo: 'Näytä sivun info'
+        showPageInfo: 'Näytä sivun info',
+        showUserGuide: 'Näytä käyttöohje',
     },
     gfi: {
         title: 'Kohdetiedot',
@@ -39,6 +42,70 @@ const fi = {
     multipleLayersWarning: 'Olet lisäämässä kartalle 10 tai enemmän karttatasoa ja se saattaa vaikuttaa palvelun suorituskykyyn',
     continue: 'Jatka',
     cancel: 'Peruuta',
+    appGuide: {
+        title: "Käyttöohje",
+        modalContent: {
+            upperBar: {
+                title: "Yläpalkin toiminnot",
+                content: {
+                    startingView: "[kuva] Palaa aloitusnäkymään Aloitusnäkymä on valmiiksi määritelty näkymä, jossa näytetään koko Suomen alueelta vesiväylät, rataverkko ja tieverkko.",
+                    sharePage: "[kuva] Jaa sivu Kartan nykyisen näkymän voi jakaa linkillä suoraan valittuun palveluun tai kopioimalla linkin. Näkymässä säilytetään kohdistus, zoom-taso sekä valitut aineistot ja valittu teema.",
+                    infoButton: "[kuva] Info Info-napin takaa löytyy tietoa sovelluksesta, käyttöohjeet sekä yhteystiedot esimerkiksi palautteen lähettämistä varten.",
+                    languageSelection: "[kuva] Kieli Karttapalvelun kieleksi voi valita suomen, ruotsin tai englannin."
+                }
+            },
+            mapLevelMenu: {
+                title: "Karttatasovalikko",
+                tabsContent: {
+                    materList: "Aineistolistausta voi suodattaa aiheittain suodatin-painikkeen takaa. " +
+                        "Karttatasoa voi hakea nimellä kirjoittamalla vähintään 3 kirjainta haku-kenttään.",
+                    themeLayerSelection: "Teeman valinta asettaa näkyviin teemaan määritellyt karttatasot\n" +
+                        "                ja avaa valikon teemaan liittyvien kartta-aineistojen valintaan.\n" +
+                        "                Osalla kartta-aineistoista on oma tyylinsä teemalle.\n" +
+                        "                Teemoja voi olla auki vain yksi kerrallaan. Teeman ollessa valittuna\n" +
+                        "                voi Kaikki tasot -valikosta valita myös muita kartta-aineistoja näkyviin\n" +
+                        "                kuin mitä teemaan on määritelty. Teema suljetaan kartan yläreunasta\n" +
+                        "                löytyvästä teema-merkistä.Teeman valinta asettaa näkyviin teemaan määritellyt\n" +
+                        "                karttatasot ja avaa valikon teemaan liittyvien kartta-aineistojen valintaan.\n" +
+                        "                Osalla kartta-aineistoista on oma tyylinsä teemalle. Teemoja voi olla auki\n" +
+                        "                vain yksi kerrallaan. Teeman ollessa valittuna voi Kaikki tasot -valikosta\n" +
+                        "                valita myös muita kartta-aineistoja näkyviin kuin mitä teemaan on määritelty.\n" +
+                        "                Teema suljetaan kartan yläreunasta löytyvästä teema-merkistä.",
+                    selectedLayers: "Lista kartalle valituista karttatasoista. Tasojen järjestystä voi vaihtaa raahaamalla niitä oikean reunan ikonista.\n" +
+                        "                Läpinäkyvyyttä voi säätää liukusäätimellä ja\n" +
+                        "                tason valinnan voi poistaa ruksista."
+                },
+                startingView: "[kuva] Palaa aloitusnäkymään Aloitusnäkymä on valmiiksi määritelty näkymä, jossa näytetään koko Suomen alueelta vesiväylät, rataverkko ja tieverkko.",
+                sharePage: "[kuva] Jaa sivu Kartan nykyisen näkymän voi jakaa linkillä suoraan valittuun palveluun tai kopioimalla linkin. Näkymässä säilytetään kohdistus, zoom-taso sekä valitut aineistot ja valittu teema.",
+                infoButton: "[kuva] Info Info-napin takaa löytyy tietoa sovelluksesta, käyttöohjeet sekä yhteystiedot esimerkiksi palautteen lähettämistä varten.",
+                languageSelection: "[kuva] Kieli Karttapalvelun kieleksi voi valita suomen, ruotsin tai englannin."
+            },
+            search: {
+                title: "Haku",
+                content: "[kuva] Haku avautuu kartan oikeaan yläkulmaan vasemman reunan suurennuslasi-painikkeesta. " +
+                    "Haku-toiminnolla voi hakea teitä ja ja osoitteita valitsemalla alasvetovalikosta " +
+                    "käytettävän haun. Hakutuloksen voi poistaa kartalta painamalla haku-kentän roskakoria."
+            },
+            measureTool: {
+                title: "Mittaustyökalut",
+                content: "[kuva] Mittaustyökalut avautuvat vasemman reunan piirto-ikonista.\n" +
+                    "                                Työkaluilla voi mitata pituutta tai pinta-alaa. mittaustulokset saa poistettua kartalta oranssilla kumi-painikkeella."
+            },
+            setFullScreen: {
+                title: "Laajenna",
+                content: "[kuva]  Laajenna-painikkeella voi laajentaa\n" +
+                    "                                Suomen Väylät -sovelluksen täyttämään\n" +
+                    "                                koko näyttöruudun."
+            },
+            zoomBar: {
+                title: "Selitteet / zoom-palkki",
+                content: "[kuva] Zoom-tason ja kartalla sillä zoom-tasolla näkyvät karttatasot selitteineen\n" +
+                    "                                saa avattua oikean alareunan selite-ikonista. Karttatasot on listattu päällimmäinen\n" +
+                    "                                ensimmäisenä. Valitsemalla zoom-palkista toisen mittakaavan,\n" +
+                    "                                siirrytään valitulle lähestymistasolle."
+            }
+        }
+    },
     appInfo: {
         title: 'Sovelluksen tiedot',
         headingText: 'Suomen Väylät -karttapalvelu kokoaa yhteen Väyläviraston avoimet, ' +
