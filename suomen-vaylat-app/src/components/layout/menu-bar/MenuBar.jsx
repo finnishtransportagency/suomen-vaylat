e5fc71730ce929e94a0474ba6b93d6b83f3b9936
tuomo-hautala <tuhautala@gmail.com>
2022-01-31 09:13:09 +0200

    Fix measuring tools translations

--- suomen-vaylat-app/src/components/layout/menu-bar/MenuBar.jsx
+++ suomen-vaylat-app/src/components/layout/menu-bar/MenuBar.jsx
@@ -114,7 +114,7 @@ const MenuBar = () => {
                 <StyledMapToolsContainer>
                     <CircleButton
                         icon={faPencilRuler}
-                        text={strings.tooltips.drawingtools.drawingtoolsButton}
+                        text={strings.tooltips.measuringTools.measuringToolsButton}
                         toggleState={isDrawingToolsOpen}
                         clickAction={closeDrawingTools}
                     />
