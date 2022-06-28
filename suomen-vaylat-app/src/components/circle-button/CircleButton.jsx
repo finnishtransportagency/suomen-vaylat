92f5229ac2abaf915d7efab68b1fa56dbf130dfc
janihyvo-sw <jani.hyvonen@sitowise.com>
2022-06-28 09:51:48 +0300

    <Circle

--- suomen-vaylat-app/src/components/circle-button/CircleButton.jsx
+++ suomen-vaylat-app/src/components/circle-button/CircleButton.jsx
@@ -1,7 +1,9 @@
 import { useState} from 'react';
 import styled from 'styled-components';
 import { motion, AnimatePresence } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { theme, isMobile } from '../../theme/theme';
 
 const StyledCircleButton = styled(motion.button)`
     border: none;
@@ -44,7 +46,7 @@ const StyledIconContainer = styled.div`
 
 const StyledCircleButtonTextContainer = styled(motion.div)`
     position: absolute;
-    left: ${props => !props.direction && 0};
+    left: ${props => props.direction === "right" && 0};
     right: ${props => props.direction === "left" && 0};
     background-color: ${props => props.theme.colors.mainWhite};
     height: 100%;
@@ -55,8 +57,8 @@ const StyledCircleButtonTextContainer = styled(motion.div)`
     white-space: nowrap;
     user-select: none;
     padding: 20px;
-    padding-left: ${props => !props.direction && "calc(100% + 16px)"};
-    padding-right: ${props => props.direction && "calc(100% + 16px)"};
+    padding-left: ${props => props.direction === "right" && "calc(100% + 16px)"};
+    padding-right: ${props => props.direction === "left" && "calc(100% + 16px)"};
     overflow: hidden;
     border-radius: 24px;
     color: ${props => props.theme.colors.mainColor1};
@@ -76,26 +78,41 @@ const CircleButton = ({
     toggleState,
     clickAction,
     tooltipDirection,
+    tooltipBackgroundColor = theme.colors.mainColor1,
+    tooltipColor = theme.colors.mainWhite,
     type,
     color,
     disabled,
+    effect = "float",
     children
 }) => {
+
     const [isHovered, setHovered] = useState(false);
+    const useReactTooltip = tooltipDirection !== "left" && tooltipDirection !== "right" && text;
+
     return (
+        <>
+        {useReactTooltip && 
+            <ReactTooltip backgroundColor={tooltipBackgroundColor} textColor={tooltipColor} disable={isMobile} id={text + "_id"} place={tooltipDirection} type='dark' effect={effect}>
+                <span>{text}</span>
+            </ReactTooltip>
+        }
+
         <StyledCircleButton
             onClick={() => clickAction()}
-            onHoverStart={() => {
-                setHovered(true);
+            onHoverStart={() => { 
+                !useReactTooltip && setHovered(true);
             }}
-            onHoverEnd={() => {
-                setHovered(false)
+            onHoverEnd={() => { 
+                !useReactTooltip && setHovered(false)
             }}
             toggleState={toggleState}
             variants={variants}
             type={type}
             color={color}
             disabled={disabled}
+            data-tip
+            data-for={text + "_id"}
         >
             {
                 icon && <StyledIconContainer>
@@ -139,6 +156,7 @@ const CircleButton = ({
                 children && children
             }
         </StyledCircleButton>
+        </>
     )
 };
 
--- suomen-vaylat-app/src/components/layout/menu-bar/MenuBar.jsx
+++ suomen-vaylat-app/src/components/layout/menu-bar/MenuBar.jsx
@@ -139,6 +139,7 @@ const MenuBar = () => {
                     icon={faLayerGroup}
                     text={strings.layerlist.layerlistLabels.mapLayers}
                     toggleState={isSideMenuOpen}
+                    tooltipDirection={"right"}
                     clickAction={() =>
                         store.dispatch(setIsSideMenuOpen(!isSideMenuOpen))
                     }
@@ -149,6 +150,7 @@ const MenuBar = () => {
                     icon={faMapMarkedAlt}
                     text={strings.gfi.title}
                     toggleState={isGfiOpen}
+                    tooltipDirection={"right"}
                     clickAction={() => {
                         store.dispatch(setIsGfiOpen(!isGfiOpen));
                         isGfiOpen && store.dispatch(setMinimizeGfi(false));
@@ -158,6 +160,7 @@ const MenuBar = () => {
                     icon={faDownload}
                     text={strings.downloads.downloads}
                     toggleState={isGfiDownloadOpen}
+                    tooltipDirection={"right"}
                     clickAction={() =>
                         store.dispatch(setIsGfiDownloadOpen(!isGfiDownloadOpen))
                     }
@@ -177,6 +180,7 @@ const MenuBar = () => {
                             strings.tooltips.measuringTools.measuringToolsButton
                         }
                         toggleState={isDrawingToolsOpen}
+                        tooltipDirection={"right"}
                         clickAction={closeDrawingTools}
                     />
                     <DrawingTools isOpen={isDrawingToolsOpen} />
@@ -185,6 +189,7 @@ const MenuBar = () => {
                     icon={faSave}
                     text={strings.saveView.saveView}
                     toggleState={isSaveViewOpen}
+                    tooltipDirection={"right"}
                     clickAction={() =>
                         store.dispatch(setIsSaveViewOpen(!isSaveViewOpen))
                     }
@@ -193,6 +198,7 @@ const MenuBar = () => {
                     icon={isFullScreen ? faCompress : faExpand}
                     text={strings.tooltips.fullscreenButton}
                     toggleState={isFullScreen}
+                    tooltipDirection={"right"}
                     clickAction={handleFullScreen}
                 />
             </StyledMenuBar>
