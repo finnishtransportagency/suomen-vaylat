db1e987b41dd648f63e1bd3e619358f5a0c7cc25
Oskari <oskari.rintamaki@sitowise.com>
2025-05-13 11:56:12 +0300

    leaving the site warning popup made to look better for mobile devices

--- suomen-vaylat-app/src/components/warning/WarningModalContent.jsx
+++ suomen-vaylat-app/src/components/warning/WarningModalContent.jsx
@@ -3,52 +3,69 @@ import { useState } from "react";
 import strings from "../../translations";
 
 const StyledWarningModalContainer = styled.div`
-    padding: 32px 32px 16px 32px;
-    color: ${props => props.theme.colors.mainColor1};
+    padding: 32px;
+    color: ${(props) => props.theme.colors.mainColor1};
+    @media ${(props) => props.theme.device.mobileL} {
+        padding: 16px;
+    }
 `;
 
 const StyledWarningTitle = styled.p`
-
+    font-size: 20px;
+    @media ${(props) => props.theme.device.mobileL} {
+        font-size: 18px;
+    }
 `;
 
 const StyledWarningSubtitle = styled.p`
-
+    font-size: 18px;
+    @media ${(props) => props.theme.device.mobileL} {
+        font-size: 16px;
+    }
 `;
 
 const StyledWarningButtonsContainer = styled.div`
     display: flex;
     justify-content: space-around;
-    align-items: baseline;
+    align-items: center;
     padding: 18px;
     border-top: 1px solid #dee2e6;
+    @media ${(props) => props.theme.device.mobileL} {
+        flex-direction: column;
+    }
 `;
 
 const StyledActionButton = styled.button`
     min-height: 48px;
     min-width: 90px;
+    margin: 8px;
     display: flex;
     justify-content: center;
     align-items: center;
     cursor: pointer;
-    background-color: ${props => props.theme.colors.mainColor1};
+    background-color: ${(props) => props.theme.colors.mainColor1};
     border-radius: 30px;
-    padding: 8px 0px 8px 0px;
+    padding: 8px;
     box-shadow: 0px 3px 6px 0px rgba(0,0,0,0.16);
     border: none;
     color: white;
-    padding: 8px 16px 8px 16px;
+
+    @media ${(props) => props.theme.device.mobileL} {
+        width: 80%;
+    }
 `;
 
 const CheckboxContainer = styled.div`
     margin-bottom: 10px;
     input[type="checkbox"] {
-      margin-right: 10px;
-  }
-  `;
+        margin-right: 10px;
+    }
+    @media ${(props) => props.theme.device.mobileL} {
+        margin-bottom: 12px;
+    }
+`;
 
-const WarningModalContent = ({
-    warning
-}) => {
+const WarningModalContent = ({ warning }) => {
     const [dontShowAgain, setDontShowAgain] = useState(false);
 
     const handleConfirm = (event) => {
@@ -57,36 +74,30 @@ const WarningModalContent = ({
         warning.confirm.action();
     };
 
-    return <StyledWarningModalContainer>
-        <StyledWarningTitle>{warning.title && warning.title}</StyledWarningTitle>
-        <StyledWarningSubtitle>{warning.subtitle && warning.subtitle}</StyledWarningSubtitle>
-        <StyledWarningButtonsContainer>
-            {  warning.dontShowAgain &&
-                <CheckboxContainer>
-                    <input type="checkbox" id="dontShowAgain" checked={dontShowAgain} onChange={() => setDontShowAgain(!dontShowAgain)}/>
-                    <label for="dontShowAgain">{strings.general.dontShowAgain}</label>
-
-                </CheckboxContainer>
-            }
-            {
-                warning.cancel &&
-                <StyledActionButton
-                    onClick={() => warning.cancel.action()}
-                >
-                    {warning.cancel.text}
-                </StyledActionButton>
-            }
-            {
-                warning.confirm &&
-                <StyledActionButton
-                    onClick={handleConfirm}
-                >
-                     {warning.confirm.text}
-                </StyledActionButton>
-            }
-        </StyledWarningButtonsContainer>
-
-    </StyledWarningModalContainer>
+    return (
+        <StyledWarningModalContainer>
+            <StyledWarningTitle>{warning.title && warning.title}</StyledWarningTitle>
+            <StyledWarningSubtitle>{warning.subtitle && warning.subtitle}</StyledWarningSubtitle>
+            <StyledWarningButtonsContainer>
+                { warning.dontShowAgain &&
+                    <CheckboxContainer>
+                        <input type="checkbox" id="dontShowAgain" checked={dontShowAgain} onChange={() => setDontShowAgain(!dontShowAgain)}/>
+                        <label htmlFor="dontShowAgain">{strings.general.dontShowAgain}</label>
+                    </CheckboxContainer>
+                }
+                { warning.cancel &&
+                    <StyledActionButton onClick={() => warning.cancel.action()}>
+                        {warning.cancel.text}
+                    </StyledActionButton>
+                }
+                { warning.confirm &&
+                    <StyledActionButton onClick={handleConfirm}>
+                        {warning.confirm.text}
+                    </StyledActionButton>
+                }
+            </StyledWarningButtonsContainer>
+        </StyledWarningModalContainer>
+    );
 };
 
 export default WarningModalContent;
