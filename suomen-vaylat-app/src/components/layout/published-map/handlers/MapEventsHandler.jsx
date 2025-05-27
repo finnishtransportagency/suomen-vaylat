e6c8c89ce821b200720ce9400c7c7bcf7e186e05
Oskari <oskari.rintamaki@sitowise.com>
2025-05-27 13:54:21 +0300

    fix map events handler

--- suomen-vaylat-app/src/components/layout/published-map/handlers/MapEventsHandler.jsx
+++ suomen-vaylat-app/src/components/layout/published-map/handlers/MapEventsHandler.jsx
@@ -6,7 +6,8 @@ import {
   resetGFILocations,
   setGFILocations,
   setPointInfo,
-  setVKMData
+  setVKMData,
+  setGFICroppingArea
 } from '../../../../state/slices/rpcSlice';
 import {
   addToDrawToolMarkers,
@@ -33,60 +34,101 @@ const MapEventsHandler = ({ channel, store }) => {
     store.dispatch(
       setPointInfo({ lon: data.coordinates.x, lat: data.coordinates.y })
     );
-    if (data.vkm !== null && store.getState().ui.activeSelectionTool === null) {
+    if (
+      data.vkm !== null &&
+      store.getState().ui.activeSelectionTool === null &&
+      store.getState().ui.activeTool === null &&
+      store.getState().ui.selectedMarker !== 7
+    ) {
       store.dispatch(setVKMData(data));
     }
     if (
       store.getState().ui.activeTool === strings.tooltips.drawingTools.marker
     ) {
+      let marker_id = data.coordinates.x + data.coordinates.y + '_id';
+      const customMarker = {
+        x: data.coordinates.x,
+        y: data.coordinates.y,
+        markerId: marker_id,
+        shape: store.getState().ui.selectedMarker,
+        msg: store.getState().ui.markerLabel,
+        color: theme.colors.mainColor2,
+        size: 5,
+        offsetX: 0,
+        offsetY: 7
+      };
       if (store.getState().ui.selectedMarker !== 7) {
-        const markerId = `${data.coordinates.x}_${data.coordinates.y}_id`;
-        const customMarker = {
-          x: data.coordinates.x,
-          y: data.coordinates.y,
-          markerId,
-          shape: store.getState().ui.selectedMarker,
-          msg: store.getState().ui.markerLabel,
-          color: theme.colors.mainColor2
-        };
         store.dispatch(addToDrawToolMarkers(customMarker));
-        store.dispatch(addMarkerRequest(customMarker));
+        store.getState().ui.selectedMarker !== 7 &&
+          store.dispatch(addMarkerRequest(customMarker));
       }
     }
   });
 
   channel.handleEvent('MapClickedEvent', () => {
+    // TODO: have every mapclick put in a new marker, it's not working atm for some reason
     store.dispatch(setVKMData(null));
     store.dispatch(removeMarkerRequest({ markerId: 'VKM_MARKER' }));
     store.dispatch(resetGFILocations([]));
   });
 
   channel.handleEvent('DataForMapLocationEvent', (data) => {
+    if (data.content && data.content.features) {
+      data.content.features.forEach((f) => {
+        if (f.properties) {
+          Object.keys(f.properties).forEach((k) => {
+            if (typeof f.properties[k] === 'object') {
+              f.properties[k] = JSON.stringify(f.properties[k]);
+            }
+          });
+        }
+      });
+    }
+
+    // reformat data to same way croppings are
+    // might need to be 'fixed' later
+    const features = data.content;
+    let geojson = { geojson: features };
+    let reformattedData = {};
+    reformattedData.content = [geojson];
+    data.content = reformattedData.content;
+
     if (
       store.getState().ui.activeSelectionTool === null &&
       store.getState().ui.activeTool === null
     ) {
-      channel.postRequest('MapModulePlugin.RemoveFeaturesFromMapRequest', [
-        null,
-        null,
-        GFI_GEOMETRY_LAYER_ID
-      ]);
+      channel &&
+        channel.postRequest('MapModulePlugin.RemoveFeaturesFromMapRequest', [
+          null,
+          null,
+          GFI_GEOMETRY_LAYER_ID
+        ]);
+
+      var MARKER_ID = 'VKM_MARKER';
 
       store.dispatch(
         addMarkerRequest({
           x: data.x,
           y: data.y,
-          markerId: 'VKM_MARKER',
+          markerId: MARKER_ID,
           shape:
-            '<svg xmlns="http://www.w3.org/2000/svg" fill="#0064af" viewBox="0 0 384 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/></svg>',
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="#0064af" viewBox="0 0 384 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/></svg>',
           size: 5,
           offsetX: 13,
           offsetY: 7
         })
       );
+      const croppingArea = {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [data.x, data.y]
+        }
+      };
 
+      store.dispatch(setGFICroppingArea(croppingArea));
       store.getState().ui.minimizeGfi && store.dispatch(setMinimizeGfi(false));
-      store.dispatch(setIsGfiOpen(true));
+      !store.getState().ui.isGfiOpen && store.dispatch(setIsGfiOpen(true));
       store.dispatch(setGFILocations(data));
     }
   });
