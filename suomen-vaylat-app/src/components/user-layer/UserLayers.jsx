17036afb701a29692f01b9fdf68b7f068f16a458
Oskari Rintamäki <oskari.rintamaki@sitowise.com>
2025-10-03 09:37:22 +0000

    Squashed commit of the following
    commit 8f17df46 
    Author: Oskari <oskari.rintamaki@sitowise.com> 
    Date: Tue Sep 30 2025 14:52:48 GMT+0300 (Eastern European Summer Time) 
    
        show no userlayers text if in extranet, and if no userlayers on SV then dont show the dropdown at all

--- suomen-vaylat-app/src/components/user-layer/UserLayers.jsx
+++ suomen-vaylat-app/src/components/user-layer/UserLayers.jsx
@@ -1,6 +1,15 @@
+import strings from "../../translations";
 import UserLayer from "./UserLayer";
 
 export const UserLayers = ({ layers }) => {
+
+  if (!layers || layers.length === 0) {
+    return (
+      <p role="status" aria-live="polite" style={{ margin: 0, padding: '8px 12px', color: '#666' }}>
+        {strings.layerlist.userContent.userlayers.noUserlayers}
+      </p>
+    );
+  }
   return (
     <>
       {layers.map((layer, index) => {
