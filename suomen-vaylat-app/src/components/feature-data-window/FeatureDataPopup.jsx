5ec8e745cbd89c60f7eced743e8c084e5ab330d6
Oskari <osqq95@gmail.com>
2025-12-15 11:30:27 +0200

    make sure objects are valid before trying to find something inside them

--- suomen-vaylat-app/src/components/feature-data-window/FeatureDataPopup.jsx
+++ suomen-vaylat-app/src/components/feature-data-window/FeatureDataPopup.jsx
@@ -581,7 +581,7 @@ export const FeatureDataPopup = () => {
           LAYER_ID
         ]);
     } else {
-      filteredFeatures[0].geometry.type === 'Point' &&
+      filteredFeatures[0].geometry?.type === 'Point' &&
         store.dispatch(removeMarkerRequest({ markerId: 'VKM_MARKER' }));
 
       let featureStyle = {
@@ -683,7 +683,7 @@ export const FeatureDataPopup = () => {
 
       data &&
         data?.content?.forEach((cont) => {
-          var featureCells = cont.geojson.features
+          var featureCells = cont.geojson?.features
             ? cont.geojson.features
                 .filter((feature) =>
                   filterFeature(feature, data, filters, channel)
