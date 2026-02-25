e87c60092dda63f116dd62159eedbe5ca3c3cdfa
Oskari <osqq95@gmail.com>
2026-02-25 14:49:46 +0200

    no need for backdrop on downloads and allow drag

--- suomen-vaylat-app/src/components/feature-data-window/dialog/FeatureDataDownloadDialog.jsx
+++ suomen-vaylat-app/src/components/feature-data-window/dialog/FeatureDataDownloadDialog.jsx
@@ -34,9 +34,9 @@ const FeatureDataDownloadDialog = ({ constraintsRef }) => {
       constraintsRef={
         constraintsRef
       } /* Reference div for dialog drag boundaries */
-      drag={false} /* Enable (true) or disable (false) drag */
+      drag={true} /* Enable (true) or disable (false) drag */
       resize={false}
-      backdrop={true} /* Is backdrop enabled (true) or disabled (false) */
+      backdrop={false} /* Is backdrop enabled (true) or disabled (false) */
       fullScreenOnMobile={
         true
       } /* Scale dialog full width / height when using mobile device */
